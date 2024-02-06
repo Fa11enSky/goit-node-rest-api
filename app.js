9d52c33a909473894cce7c5f12aa5e8bbdad8273
Fa11enSky <133168352+Fa11enSky@users.noreply.github.com>
2024-02-06 14:34:46 +0200

    change routes

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 
 const contactsRouter = require("./routes/contactsRouter");
 const authRouter = require("./routes/auth");
-const currentRouter = require("./routes/users");
 
 require("dotenv").config();
 
@@ -14,9 +13,7 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", authRouter);
-
-app.use("/users", currentRouter);
+app.use("/users", authRouter);
 
 app.use("/api/contacts", contactsRouter);
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const { validateBody } = require("../middlewares");
+const authenticate = require("../middlewares/authenticate");
+
 const { registerSchema, loginSchema } = require("../schemas");
-const { register, login,getCurrent } = require("../controllers");
+const { register, login,getCurrent,logout } = require("../controllers");
 
 const router = express.Router();
 
@@ -9,5 +11,8 @@ router.post("/register", validateBody(registerSchema), register);
 
 router.post("/login", validateBody(loginSchema), login);
 
+router.get("/current", authenticate, getCurrent);
+
+router.post("/logout", authenticate, logout);
 
 module.exports = router;
